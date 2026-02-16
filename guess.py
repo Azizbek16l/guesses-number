9f071ea423f01122f8d659f469ba27c817640fcb
Azizbek Topilboyev <azizbektopilboyev7@gmail.com>
2026-02-17 03:10:23 +0500

    cleaned

--- guess.py
+++ guess.py
@@ -3,7 +3,7 @@ n=random.randint(1,10)
 guesses=0
 while True:
     guess=int(input('1 dan 10gacha son kirit '))
-    guesses+=1
+    
     if n==guess:
         print('topding🎉')
         break
@@ -14,5 +14,3 @@ while True:
         print('yutqazdingiz')
         break
         
-
-    
