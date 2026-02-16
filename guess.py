7331258e9a83d8a63fc18c06ee19f9f45982a7b2
Azizbek Topilboyev <azizbektopilboyev7@gmail.com>
2026-02-16 22:59:08 +0500

    repeat untill win user

--- guess.py
+++ guess.py
@@ -1,10 +1,13 @@
 import random
 n=random.randint(1,10)
-guess=int(input('1 dan 10gacha son kirit '))
 
-if n==guess:
-    print('topding')
-else:
-    print('topa olmading')
-    
+while True:
+    guess=int(input('1 dan 10gacha son kirit '))
+    if n==guess:
+        print('topding🎉')
+        break
+    else:
+        print('xato')
+        
+
     
